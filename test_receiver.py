47f97b9d2b551469b4ddfaa175cf02e93336ee10
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 19:34:45 +0530

    Update test_receiver.py

--- test_receiver.py
+++ test_receiver.py
@@ -4,7 +4,8 @@ import receiver
 class receiver_test(unittest.TestCase):
 
     def test_GetDataFromConsoleSenderOutput(self):
-        receiver.GetDataFromConsoleSenderOutput()
+        SampleSizeForSMA = 5
+        receiver.GetDataFromConsoleSenderOutput(SampleSizeForSMA)
 
 if __name__ == '__main__':
   unittest.main()
