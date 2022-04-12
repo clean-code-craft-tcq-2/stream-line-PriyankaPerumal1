9ddb6e298a7edb91735d770ff8b5e1878ed850b2
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 19:17:16 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,23 +2,64 @@ import sys
 from signal import signal, SIGPIPE, SIG_DFL 
 #Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html) 
 
+BMSparameters = ["current", "temperature"]
 
-def readFromConsole():
+def readDataFromConsole():
     signal(SIGPIPE,SIG_DFL)
     lines = sys.stdin.readlines()
     return lines
 
-def formulateReadings(stream):
-  mergedReadings = []
-  for csvReading in stream:
-      csvReading = csvReading.strip('\n')
-      reading = list(map(float,csvReading.split(',')))
-      mergedReadings.append(reading)
-  return mergedReadings
-  
+def formulateReadingsToProperFormat(stream):
+  ArrayOfReadings = []
+  for Reading in stream:
+      Reading = Reading.strip('\n')
+      readingList = list(map(float,Reading.split(',')))
+      ArrayOfReadings.append(readingList)
+  return ArrayOfReadings
+
+def getParamindex(parameter):
+    for index, parameterName in enumerate(BMSparameters):
+        if parameterName == parameter:
+            return index
+        
+def extractParameterReading(ArrayOfReadings, param):
+    ParamIndex = getParamindex(param)
+    return [readings[ParamIndex] for readings in ArrayOfReadings]
+
+def createWindow(readings, windowSize):
+    windows = [readings[index : index + windowSize] for index, value in enumerate(readings) if index < len(readings) - windowSize + 1]
+    return windows
+
+def roundOffAverage(value, digits):
+    return round(value, digits)
+
+def calculateAverage(array):
+    return calculateSum(array) / len(array)
+
+def calculateSum(array):
+    return sum(array)
+
+def calculateMovingAverage(readings, windowSize):
+    windows = createWindow(readings, windowSize)
+    movingAverages = [roundOffAverage(calculateAverage(window), 2) for window in windows]
+    return movingAverages
+
+def calculateMinMaxReading(readings):
+    minReading = calculateMinReading(readings)
+    maxReading = calculateMaxReading(readings)
+    return {'min': minReading, 'max': maxReading}
+
+def calculateMinReading(readings):
+    return min(readings)
+
+def calculateMaxReading(readings):
+    return max(readings)
+
 def GetDataFromConsoleSenderOutput():
-  stream = readFromConsole()
-  mergedreadings = formulateReadings(stream)
-  print("helloo1")
-  print(mergedreadings)
-  print("helloo again")
+  datastream = readDataFromConsole()
+  ArrayOfReadings = formulateReadingsToProperFormat(datastream)
+  for param in BMSparameters:
+    readings = extractParameterReading(ArrayOfReadings, param)
+    movingAverage = calculateMovingAverage(readings, 5)
+    minMaxReading = calculateMinMaxReading(readings)
+    
