d7a8088152b9d61a3e4d884a38dd090a0fa2b0b3
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 17:12:25 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,7 +1,13 @@
-import sys
+import sys, errno
+try:
+    ### IO operation ###
+except IOError as e:
+    if e.errno == errno.EPIPE:
+        ### Handle the error ###
 
 
 def GetDataFromConsoleSenderOutput():
+  print("i am here")
   for line in sys.stdin:
      line=line.split(',')
      print(line)
