b6f457078ff9f6cd82a9238e4fc4eac20f7b143f
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 17:37:10 +0530

    Create test_receiver.py

---
+++ test_receiver.py
@@ -0,0 +1,10 @@
+import unittest
+import receiver
+
+class receiver_test(unittest.TestCase):
+
+    def test_GetDataFromConsoleSenderOutput(self):
+        receiver.GetDataFromConsoleSenderOutput()
+
+if __name__ == '__main__':
+  unittest.main()
