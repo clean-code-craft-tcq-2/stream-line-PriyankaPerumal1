c707934bcbe7fe9592b34c4674919e8747e74451
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-04-12 17:33:03 +0530

    Update sender_stub.py

--- sender_stub.py
+++ sender_stub.py
@@ -1,5 +1,8 @@
 from random import randint
 
+from signal import signal, SIGPIPE, SIG_DFL  
+signal(SIGPIPE,SIG_DFL) 
+
 def generate_randomReadings(minLimit,maxLimit):
     return randint(minLimit, maxLimit)
 
